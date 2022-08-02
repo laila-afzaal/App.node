1b9d2a75033e5ec4c52f8df45377abf7af808cef
Laila Afzaal <lailaafzaal06@gmail.com>
2022-08-02 22:42:32 +0500

    my commit

---
+++ node.mjs
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors';
+
+const app = express ();
+app.use(cors());
+
+app.get('/home', (req,res) => {
+  res.send('I am home page')
+})
+
+app.get('/profile', (req, res) => {
+  res.send('I am profile page')
+})
+
+app.get('/weather', (req, res) => {
+  res.send({
+    temperature: '35',
+    weather: 'cloudy',
+    city: 'karachi'
+  })
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
